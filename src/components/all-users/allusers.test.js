79c345deff3806c5777ea88b8c20e5cf3bbee985
Nick <ex-er-sis@ya.ru>
2023-06-05 10:48:51 +0300

    Forgot update tests...

--- src/components/all-users/allusers.test.js
+++ src/components/all-users/allusers.test.js
@@ -1,16 +1,32 @@
 import { render, screen } from '@testing-library/react';
+import { StoreContext } from '../index';
 import AllUsersList from './index';
 
 const testUsers = [{ username: 'username', uid: 'dfgdfg-fgjhfj', rating: 0, banned: false }];
 
+const testStore = {
+  toggleModal: () => {},
+  setControledUser: () => {},
+  resetUser: () => {},
+  banUser: () => {}
+};
+
 test('All users has next button', () => {
-  render(<AllUsersList users={testUsers} />);
+  render(
+    <StoreContext.Provider value={testStore}>
+      <AllUsersList users={testUsers} />
+    </StoreContext.Provider>
+  );
   const next = screen.getByTestId('next-button');
   expect(next).toBeInTheDocument();
 });
 
 test('All users has update button', () => {
-  render(<AllUsersList users={testUsers} />);
+  render(
+    <StoreContext.Provider value={testStore}>
+      <AllUsersList users={testUsers} />
+    </StoreContext.Provider>
+  );
   const update = screen.getByTestId('update-button');
   expect(update).toBeInTheDocument();
 });
--- src/components/user-item/user.test.js
+++ src/components/user-item/user.test.js
@@ -1,16 +1,32 @@
 import { render, screen } from '@testing-library/react';
+import { StoreContext } from '../index';
 import UserItem from './index';
 
 const testUser = { username: 'username', uid: 'dfgdfg-fgjhfj', rating: 0, banned: false };
 
+const testStore = {
+  toggleModal: () => {},
+  setControledUser: () => {},
+  resetUser: () => {},
+  banUser: () => {}
+};
+
 test('Test single user item render', () => {
-  render(<UserItem user={testUser} />);
+  render(
+    <StoreContext.Provider value={testStore}>
+      <UserItem user={testUser} />
+    </StoreContext.Provider>
+  );
   const userItem = screen.getByRole('listitem');
   expect(userItem).toBeInTheDocument();
 });
 
 test('Rated user rating displayed', () => {
-  render(<UserItem user={testUser} showRating={true} />);
+  render(
+    <StoreContext.Provider value={testStore}>
+      <UserItem user={testUser} showRating={true} />
+    </StoreContext.Provider>
+  );
   const rating = screen.getByTestId('user-rating-count');
   expect(rating).toBeInTheDocument();
 });
